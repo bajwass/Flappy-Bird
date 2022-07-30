592bed8db38aaae3aff337cd4428953f8142d65c
Sumeet <sumeet-bajwa-34@live.ca>
2022-07-30 15:43:56 -0700

    Update game.js
    added functions

--- game.js
+++ game.js
@@ -3,8 +3,14 @@ const cvs = document.getElementById("bird");
 const ctx = cvs.getContext("2d");
 
 //Game VARS AND CONSTS
+let frames = 0;
 
 //DRAW
+function draw(){
 
+}
 
 //UPDATE
+function update(){
+    
+}
