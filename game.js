b7dd46432315c8a1292361c872ebed9da53966d0
Sumeet <sumeet-bajwa-34@live.ca>
2022-07-30 16:04:17 -0700

    Update game.js
    created the animation for the flappy bird itself:
    
    bird now shows on the html file.

--- game.js
+++ game.js
@@ -25,6 +25,31 @@ const bg = {
 
     }
 }
+//BIRD
+const bird = {
+    animation : [
+        {sX: 276, sY: 112},
+        {sX: 276, sY: 139},
+        {sX: 276, sY: 164},
+        {sX: 276, sY: 139}
+
+    ],
+    x: 50,
+    y: 150,
+    w: 34,
+    h: 26,
+
+    frame: 0,
+
+    draw: function (){
+        let bird = this.animation[this.frame];
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+
+    }
+
+
+
+}
 
 //FOREGROUND
 const fg = {
@@ -50,6 +75,7 @@ function draw(){
 
     bg.draw();
     fg.draw();
+    bird.draw();
 
 }
 
