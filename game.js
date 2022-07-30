4ca4c329681ec8e0e366c3b815dee3fc829a5d92
Sumeet <sumeet-bajwa-34@live.ca>
2022-07-30 15:53:30 -0700

    Update game.js
    created the sonst that holds all the variable for the background

--- game.js
+++ game.js
@@ -9,6 +9,20 @@ let frames = 0;
 const sprite = new Image();
 sprite.src = "img/sprite.png"
 
+//BACKGROUND
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0, 
+    y: cvs.height -226,
+
+    draw: function (){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.y, this.w, this.h);
+    }
+}
+
 //DRAW
 function draw(){
     ctx.fillStyle="#70c5ce";
